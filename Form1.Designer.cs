ad5803cc5bba5e6cd3934c00b2e323a379b7acdf
CodedByVin <kgosanamatomevincent@gmail.com>
2026-09-21 13:24:42 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SERIALIZING_OBJECTS_IN_C__PT._1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbArtists = new System.Windows.Forms.RichTextBox();
+            this.btnSerialize = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbArtists
+            // 
+            this.rtbArtists.Location = new System.Drawing.Point(68, 93);
+            this.rtbArtists.Name = "rtbArtists";
+            this.rtbArtists.Size = new System.Drawing.Size(305, 300);
+            this.rtbArtists.TabIndex = 0;
+            this.rtbArtists.Text = "";
+            // 
+            // btnSerialize
+            // 
+            this.btnSerialize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSerialize.Location = new System.Drawing.Point(68, 399);
+            this.btnSerialize.Name = "btnSerialize";
+            this.btnSerialize.Size = new System.Drawing.Size(94, 38);
+            this.btnSerialize.TabIndex = 1;
+            this.btnSerialize.Text = "Serialize";
+            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.Click += new System.EventHandler(this.btnSerialize_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(268, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Deserialize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(168, 400);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 38);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(149, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "My Top 5 Artists";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter the names of your artists:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSerialize);
+            this.Controls.Add(this.rtbArtists);
+            this.Name = "Form1";
+            this.Text = "Serialization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbArtists;
+        private System.Windows.Forms.Button btnSerialize;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; // Import the System.IO namespace -> Contains the FileStream class.
+using System.Runtime.Serialization; 
+using System.Runtime.Serialization.Formatters.Binary; // Import the Serialization namespaces -> contains the neccessary tools to serialize and deserialize objects.
+
+namespace SERIALIZING_OBJECTS_IN_C__PT._1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = new FileStream("artists.binary", FileMode.Create)) // Creating an instance of the FileStream class.
+            {                                                                         // This instance will create a file on the hardware to write the binary data of the serialized objects to it.
+                string artists = rtbArtists.Text;
+
+                // The Binary Formatter is the actual tool that translates the object' state into binary data.
+                // With the .Serialize() method, we pass in the FileStream object and the object we want to serialize.
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                bFormatter.Serialize(fs,artists);
+
+                MessageBox.Show("Artists saved successfully!","Data Saved");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(FileStream fs = new FileStream("artists.binary", FileMode.Open)) // Here the FileStream tool opens the existing file to deserialize.
+            {
+                // We will need the Binary Formatter to translate the binay data from a file to an actual object.
+                // We use the .Deserialize() method to deserialize the object.
+                BinaryFormatter bFormatter = new BinaryFormatter();
+
+                // The .Deserialize () method returns an object. This object must be cast to the appropriate type.
+                string artists = (string)bFormatter.Deserialize(fs);
+
+                rtbArtists.Text = artists;
+
+                MessageBox.Show("Artists loaded successfully!","Data Loaded");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbArtists.Clear();
+        }
+    }
+}
